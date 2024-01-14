4d2c046ade14a2dce495a98035f8fea049ca2634
chemcarl08 <chemcarl08@hotmail.com>
2024-01-13 22:39:39 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magic)
+        {
+            Console.Write("What is the guess? ");
+            string guessnumber = Console.ReadLine();
+            guess = int.Parse(guessnumber);
+
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
