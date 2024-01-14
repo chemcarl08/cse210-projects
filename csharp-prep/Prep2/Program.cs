4a6a83dcfc56f660b91bc0643db090cb634452c6
chemcarl08 <chemcarl08@hotmail.com>
2024-01-13 21:09:02 -0600

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage);
+
+        string lettergrade = "";
+
+        if (percent >= 90)
+        {
+            lettergrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            lettergrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            lettergrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            lettergrade = "D";
+        }
+        else
+        {
+            lettergrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {lettergrade}.");
+
+        if (percent >=70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Failed for now but you can do it next time!");
+        }
+
     }
 }
